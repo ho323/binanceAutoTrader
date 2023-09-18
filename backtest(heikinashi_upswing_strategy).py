9db2a44ab8a2b42e0a7eb2ba5f2d65ae73fd245d
Hoseong Kim <8536048@gmail.com>
2023-09-19 03:02:22 +0900

    백테스트

--- backtest(heikinashi_upswing_strategy).py
+++ backtest(heikinashi_upswing_strategy).py
@@ -2,10 +2,12 @@ from BinanceTrade import *
 from binance.client import Client
 import pandas as pd
 import numpy as np
-import requests
 import datetime
 import yaml
 import openpyxl
+import time
+
+start_time = time.time()
 
 with open('/Users/ho/Documents/config.yaml', encoding='UTF-8') as f:
     _cfg = yaml.load(f, Loader=yaml.FullLoader)
@@ -16,19 +18,20 @@ client = Client(api_key=APP_KEY, api_secret=APP_SECRET)
 
 # 코인 설정
 ticker = "ETHUSDT"
-interval='30m'   # 1m, 3m, 5m, 15m, 30m, 1h, 2h, 4h, 6h, 8h, 12h, 1d, 3d, 1w, 1M
-df = get_ohlcv(client,ticker,interval)
+interval='1h'   # 1m, 3m, 5m, 15m, 30m, 1h, 2h, 4h, 6h, 8h, 12h, 1d, 3d, 1w, 1M
+limit=1000
+df = get_ohlcv(client,ticker,interval,limit)
 
 # 초기 설정
 start_balance =100000.0 # 시작 자산
 cash = start_balance
 fee = 0.0005
-rate = 1
+rate = 0.1
 
 # 전략 설정
 bt = heikinashi_upswing_strategy(df,13,21)
-bt[['현금', '보유수량', '총 자산', '수익률']] = 0.0
 
+bt[['현금', '보유수량', '총 자산', '수익률']] = 0.0
 quantity = 0
 bt.loc[0, '현금'] = cash
 bt.loc[0, '총 자산'] = cash
@@ -48,12 +51,17 @@ for i in range(1, len(bt)):
     bt.loc[i, '수익률'] = (bt.loc[i,'총 자산'] - start_balance) / start_balance
 bt['수익률'] *= 100
 
-print("매수 횟수: ", len(bt[bt['Action']=='Buy']), "번")
-print("매도 횟수: ", len(bt[bt['Action']=='Sell']), "번")
-print("최고 수익률: ", round(max(bt['수익률']),3), "%")
-print("MDD: ", round(min(bt['수익률']),3), "%")
-print("최종 수익률: ", round(bt['수익률'].iloc[-1],3), "%")
+end_time = time.time()
+execution_time = end_time - start_time
+print(f"실행 시간: {execution_time:.2f} 초")
+
+
+print("매수 횟수:", len(bt[bt['Action']=='Buy']), "번")
+print("매도 횟수:", len(bt[bt['Action']=='Sell']), "번")
+print("최고 수익률:", round(max(bt['수익률']),3), "%")
+print("MDD:", round(min(bt['수익률']),3), "%")
+print("최종 수익률:", round(bt['수익률'].iloc[-1],3), "%")
 
 # 엑셀로 출력
 now = datetime.datetime.now()
-bt.to_excel(f"BackTest[{now.strftime('%Y-%m-%d %H:%M:%S')}].xlsx")
+bt.to_excel(f"backtest/BackTest[{now.strftime('%Y-%m-%d %H:%M:%S')}].xlsx")
