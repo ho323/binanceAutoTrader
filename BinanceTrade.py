991af17f5de06453a5821b1ea1630e3d1807d656
Hoseong Kim <8536048@gmail.com>
2023-09-19 03:02:49 +0900

    보조지표, 전략 추가

--- BinanceTrade.py
+++ BinanceTrade.py
@@ -14,7 +14,7 @@ def send_message(DISCORD_WEBHOOK_URL, msg):
     print(message)
 
 def get_balance(client, asset='USDT'):
-    """잔고 조회"""
+    """잔고 조회"""    
     balances = client.get_asset_balance(asset=asset)
     if balances['free'] is not None:
         return balances['free']+' USDT'
@@ -25,10 +25,11 @@ def get_current_price(client, symbol="BTCUSDT"):
     """현재가 조회"""
     return client.get_symbol_ticker(symbol=symbol)['price']
 
-def get_ohlcv(client, ticker="BTCUSDT", interval="1d"):
+def get_ohlcv(client, ticker="BTCUSDT", interval="1d",limit=100):
     data = client.get_historical_klines(
         symbol=ticker,
         interval=interval,
+        limit=limit
     )
     colums = ['Time', 'Open', 'High', 'Low', 'Close', 'Volume', 'Close Time', 'Quote asset volume', 'Number of trades', 'Taker buy base asset volume', 'Taker buy quote asset volume', 'Ignore']
     df = pd.DataFrame(data, columns=colums)
@@ -37,6 +38,55 @@ def get_ohlcv(client, ticker="BTCUSDT", interval="1d"):
     df['Time'] = pd.to_datetime(df['Time'], unit='ms')
     # df.set_index('Time', inplace=True)
     return df
+
+def get_all_ohlcv(symbol="BTCUSDT", interval="1d"):
+    # Binance API endpoint URL
+    url = "https://api.binance.com/api/v1/klines"
+
+    # Convert start date to milliseconds (2016년 6월 1일)
+    start_date = datetime.datetime(2016, 6, 1)
+    start_time = int(start_date.timestamp()) * 1000
+
+    # Get the current date and time as the end date
+    end_date = datetime.datetime.now()
+    end_time = int(end_date.timestamp()) * 1000
+
+    params = {
+        "symbol": symbol,
+        "interval": interval,
+        "limit": 1000,      # Maximum limit per request
+        "startTime": start_time,
+        "endTime": end_time
+    }
+
+    # List to store data
+    data_list = []
+
+    while True:
+        response = requests.get(url, params=params)
+        data = response.json()
+        
+        if len(data) == 0:
+            break
+        
+        data_list.extend(data)
+        
+        # Update the start time for the next request
+        params["startTime"] = data[-1][0] + 1
+
+    # Convert the response data into a DataFrame
+    df = pd.DataFrame(data_list, columns=["Time", "Open", "High", "Low", "Close", "Volume", "close_time", "quote_asset_volume", "number_of_trades", "taker_buy_base_asset_volume", "taker_buy_quote_asset_volume", "ignore"])
+    df["Time"] = pd.to_datetime(df["Time"], unit="ms")
+
+    # Select only the relevant columns (OHLCV)
+    df = df[["Time", "Open", "High", "Low", "Close", "Volume"]]
+
+    # Convert the OHLCV values to numeric
+    df[["Open", "High", "Low", "Close", "Volume"]] = df[["Open", "High", "Low", "Close", "Volume"]].apply(pd.to_numeric)
+
+    # Print the DataFrame with the OHLCV data
+    return df
+
 
 def get_heikinashi(df):
     x = pd.DataFrame()
@@ -100,6 +150,28 @@ def get_bb(df):
     x['BB Lower'] = x['BB Middle'] - 2 * std
     return x
 
+def get_mfi(data, period=14):
+    """MFI 계산"""
+    typical_price = (data['High'] + data['Low'] + data['Close']) / 3
+    raw_money_flow = typical_price * data['Volume']
+    
+    money_flow_positive = raw_money_flow.where(data['Close'] > data['Close'].shift(1), 0)
+    money_flow_negative = raw_money_flow.where(data['Close'] < data['Close'].shift(1), 0)
+    
+    money_ratio = money_flow_positive.rolling(window=period).sum() / money_flow_negative.rolling(window=period).sum()
+    mfi = 100 - (100 / (1 + money_ratio))
+    
+    return mfi
+
+def get_rmf(data):
+    """RMF 계산"""
+    return (get_rsi(data) + get_mfi(data)) / 2
+
+def get_vwma(data, period=14):
+    """VWMA 계산"""
+    data['Weighted_Price'] = data['Close'] * data['Volume']
+    return data['Weighted_Price'].rolling(window=period).sum() / data['Volume'].rolling(window=period).sum()
+
 # 거래 전략
 def ma_cross(df,s=20,l=120):
     x = df.copy()
@@ -146,3 +218,39 @@ def grid_strategy(df):
 def torres_strategy(df):
     """토레스 전략"""
     pass
+
+def srp_strategy(data, rmf_threshold=30, dca_levels=[0.1,0.2,0.5], std_dev=2):
+    """
+    SRP 전략
+    매수 조건: VWMA 밴드 하단 아래, RMF 입력한 지정값 아래
+    매도 조건: VWMA 밴드 상단 위, RMF 입력한 지정값 위
+    DCA 접근 방식: 1차, 2차, 3차 분할매수 후 전량매도
+    std_dev: 표준 편차 값 (상단 및 하단 밴드의 폭을 결정)
+    """
+    data['VWMA'] = get_vwma(data)
+    data['RMF'] = get_rmf(data)
+    data['Upper_Band'] = data['VWMA'] + (std_dev * data['VWMA'].rolling(window=14).std())
+    data['Lower_Band'] = data['VWMA'] - (std_dev * data['VWMA'].rolling(window=14).std())
+
+    actions = []
+    dca_counter = 0  # DCA 수행 횟수
+    for index, row in data.iterrows():
+        price = row['Close']
+        vwma = row['VWMA']
+        rmf = row['RMF']
+        rmf_upper = row['Upper_Band']
+        rmf_lower = row['Lower_Band']
+        if price < rmf_lower and rmf < rmf_threshold:
+            if dca_counter < len(dca_levels):
+                dca_counter += 1
+                actions.append('Buy')
+            else:
+                actions.append('Stay')
+        elif price > rmf_upper and rmf > 100-rmf_threshold:
+            dca_counter = 0
+            actions.append('Sell')
+        else:
+            actions.append('Stay')
+    
+    data['Action'] = actions
+    return data
