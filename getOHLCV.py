09af72d7265c2485dc63bf451fac411aaa2d867f
Hoseong Kim <8536048@gmail.com>
2023-09-22 01:54:30 +0900

    Update getOHLCV.py

--- getOHLCV.py
+++ getOHLCV.py
@@ -1,3 +1,4 @@
+import os
 import requests
 import pandas as pd
 from datetime import datetime
@@ -6,11 +7,10 @@ from datetime import datetime
 url = "https://api.binance.com/api/v1/klines"
 
 # Set the parameters for the API request
-symbols = ["BTCUSDT","ETHUSDT","XRPUSDT","BNBUSDT","ADAUSDT","DOGEUSDT"]
-interval='15m'   # 1m, 3m, 5m, 15m, 30m, 1h, 2h, 4h, 6h, 8h, 12h, 1d, 3d, 1w, 1M
+symbols = ["BTCUSDT", "ETHUSDT", "XRPUSDT", "BNBUSDT", "ADAUSDT", "DOGEUSDT"]
+interval = '15m'  # 1m, 3m, 5m, 15m, 30m, 1h, 2h, 4h, 6h, 8h, 12h, 1d, 3d, 1w, 1M
 
-
-# Convert start date to milliseconds (2016년 6월 1일)
+# Convert start date to milliseconds
 start_date = datetime(2017, 8, 15)
 start_time = int(start_date.timestamp()) * 1000
 
@@ -22,7 +22,7 @@ for symbol in symbols:
     params = {
         "symbol": symbol,
         "interval": interval,
-        "limit": 1000,      # Maximum limit per request
+        "limit": 1000,  # Maximum limit per request
         "startTime": start_time,
         "endTime": end_time
     }
@@ -33,25 +33,35 @@ for symbol in symbols:
     while True:
         response = requests.get(url, params=params)
         data = response.json()
-        
+
         if len(data) == 0:
             break
-        
+
         data_list.extend(data)
-        
+
         # Update the start time for the next request
         params["startTime"] = data[-1][0] + 1
 
     # Convert the response data into a DataFrame
-    df = pd.DataFrame(data_list, columns=["timestamp", "open", "high", "low", "close", "volume", "close_time", "quote_asset_volume", "number_of_trades", "taker_buy_base_asset_volume", "taker_buy_quote_asset_volume", "ignore"])
-    df["timestamp"] = pd.to_datetime(df["timestamp"], unit="ms")
+    df = pd.DataFrame(data_list, columns=['Time', 'Open', 'High', 'Low', 'Close', 'Volume', "close_time",
+                                          "quote_asset_volume", "number_of_trades", "taker_buy_base_asset_volume",
+                                          "taker_buy_quote_asset_volume", "ignore"])
+    df['Time'] = pd.to_datetime(df['Time'], unit='ms')
 
     # Select only the relevant columns (OHLCV)
-    df = df[["timestamp", "open", "high", "low", "close", "volume"]]
+    df = df[['Time', 'Open', 'High', 'Low', 'Close', 'Volume']]
 
     # Convert the OHLCV values to numeric
-    df[["open", "high", "low", "close", "volume"]] = df[["open", "high", "low", "close", "volume"]].apply(pd.to_numeric)
-    
+    df[['Open', 'High', 'Low', 'Close', 'Volume']] = df[['Open', 'High', 'Low', 'Close', 'Volume']].apply(pd.to_numeric)
+
+    # Create a directory if it doesn't exist
     now = datetime.now()
-    df.to_excel(f"data/{symbol}/{symbol}{interval}.xlsx")
-    print(f"{now.strftime('%Y-%m-%d %H:%M')} {symbol} 완료!")
+    df_dir = f"data/{symbol}"
+    if not os.path.exists(df_dir):
+        os.makedirs(df_dir)
+
+    # Save the data as a CSV file
+    csv_file_path = f"{df_dir}/{symbol}{interval}.csv"
+    df.to_csv(csv_file_path, index=False)
+
+    print(f"{now.strftime('%Y-%m-%d %H:%M')} {symbol} 데이터 저장 완료!")
