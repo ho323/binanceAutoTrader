3fbda2b97e7c7f530459baeddf758305e390c268
Hoseong Kim <8536048@gmail.com>
2023-09-29 19:50:42 +0900

    디스코드 바이낸스 봇

--- DiscordBinanceBot.py
+++ DiscordBinanceBot.py
@@ -2,12 +2,17 @@ import yaml
 import discord
 from discord.ext import commands
 from binancebot import BinanceBot
+from binance.exceptions import BinanceAPIException
 import tratingtool
+import pandas as pd
+import datetime
+import time
+
 
 with open('/Users/ho/Documents/config.yaml', encoding='UTF-8') as f:
     _cfg = yaml.load(f, Loader=yaml.FullLoader)
-API_KEY = _cfg['APP_KEY']
-API_SECRET = _cfg['APP_SECRET']
+API_KEY = _cfg['API_KEY']
+API_SECRET = _cfg['API_SECRET']
 DISCORD_TOKEN = _cfg['DISCORD_TOKEN']
 
 # 디스코드 봇 설정
@@ -17,21 +22,15 @@ intents.typing = False  # 사용자가 타이핑 중인 상태를 봇이 무시
 intents.presences = False  # 사용자 활동 정보를 봇이 무시
 discord_bot = commands.Bot(command_prefix='!', intents=intents)
 
+
 # 매매 초기 설정
+strategy = tratingtool.strategy_vwma_long   # 전략
 start_ticker = "DOGEUSDT"
 interval='15m'   # 1m, 3m, 5m, 15m, 30m, 1h, 2h, 4h, 6h, 8h, 12h, 1d, 3d, 1w, 1M
 dca_levels = [0.3,0.5,1.0]      # 분할매수 비율
 dcl = -0.02 # 분할매수 진입 최소 수익률 
 mtp = -1    # Min Take Profit 매도시 최소 수익률 (미사용 시 -1)
 stl = -1    # Stop Loss (미사용 시 -1)
-
-# 전략 설정
-strategy = tratingtool.strategy_vwma_long
-balance = 1000.0    # 가상 자산
-length=14
-change_percentage=1.4
-below=55
-above=100
 
 # 매매 봇
 binance_bot = BinanceBot(
@@ -47,7 +46,8 @@ binance_bot = BinanceBot(
 )
 
 async def binance_trading_bot():
-    binance_bot.trading()   # 매매 시작
+    # 매매 시작
+    await binance_bot.trading()
 
 @discord_bot.event
 async def on_ready():
@@ -58,27 +58,34 @@ async def on_command_error(ctx, error):
     await ctx.send(error)
 
 @discord_bot.command()
-async def balance(ctx):
+async def 잔고(ctx):
     # 현재 선물 USDT 잔고
-    usdt = binance_bot.get_balance()
+    usdt = await binance_bot.get_balance()
     await ctx.send(f"현재 USDT 잔고: {usdt}")
 
 @discord_bot.command()
 async def 현재포지션(ctx):
-    position = binance_bot.get_futures_position()
-    sign = 'Long' if position > 0 else 'Short' if position < 0 else ''
+    position = await binance_bot.get_futures_position()
+    position = float(position)
+    if position > 0:
+        sign = 'Long'
+    elif position < 0:
+        sign = 'Short'
+    else:
+        sign = ''
     ror = binance_bot.ror
-    await ctx.send(f"현재 포지션: {position} {sign}")
-    await ctx.send(f"수익률: {ror}")
+    await ctx.send(f"현재 포지션: {binance_bot.ticker} {position} {sign}")
+    await ctx.send(f"수익률: {ror}%")
 
 @discord_bot.command()
-async def set_coin(ctx, c: str):
+async def 코인설정(ctx, coin:str, interval:str):
     # 거래 코인 설정
-    binance_bot.ticker = c
-    await ctx.send(f'거래 코인이 {c}으로 설정되었습니다.')
+    binance_bot.ticker = coin
+    binance_bot.interval = interval
+    await ctx.send(f'거래 코인이 {coin}/{interval}으로 설정되었습니다.')
 
 @discord_bot.command()
-async def set_leverage(ctx, leverage: int):
+async def 레버리지설정(ctx, leverage: int):
     # 레버리지 설정
     binance_bot.client.futures_change_leverage(
         symbol=binance_bot.ticker,
@@ -87,9 +94,9 @@ async def set_leverage(ctx, leverage: int):
     await ctx.send(f'{binance_bot.ticker}의 레버리지가 {leverage}로 설정되었습니다.')
 
 @discord_bot.command()
-async def set_margin_type(ctx, margin_type):
+async def 마진타입설정(ctx, margin_type):
 
-    binance_bot.client.futures_change_margin_type(
+    await binance_bot.client.futures_change_margin_type(
         symbol=binance_bot.ticker,
         margin_type=margin_type,
     )
@@ -98,13 +105,14 @@ async def set_margin_type(ctx, margin_type):
 @discord_bot.command()
 async def 전략설정(ctx, st):
     binance_bot.strategy = st
-    await ctx.send(f'{binance_bot.ticker}의 선물 마진 타입이 {st}로 설정되었습니다.')
+    await ctx.send(f'매매 전략이 {st}로 설정되었습니다.')
 
 @discord_bot.command()
 async def 매매시작(ctx):
     # 매매 시작
     await ctx.send('매매를 시작합니다.')
-    await binance_trading_bot(ctx)
+    await ctx.send(f'현재 {binance_bot.ticker} 가격: {await binance_bot.get_current_price()}')
+    await binance_trading_bot()
 
 @discord_bot.command()
 async def 매매종료(ctx):
@@ -117,5 +125,6 @@ async def 매매종료(ctx):
 #     # 봇 종료
 #     await ctx.send('봇을 종료합니다.')
 #     await discord_bot.close()
+
 
 discord_bot.run(DISCORD_TOKEN)
--- binancebot.py
+++ binancebot.py
@@ -4,6 +4,7 @@ import pandas as pd
 import datetime
 import time
 
+
 class BinanceBot():
     def __init__(self, key, secret, ticker, interval, strategy, dca_levels, dcl, mtp, stl):
         self.API_KEY = key
@@ -16,7 +17,7 @@ class BinanceBot():
         self.dca_level = 0
         self.buy_price = None   # 평균단가
         self.pre_price = None   # 직전 매수가
-        self.ror = 0        # 평균단가 대비 수익률
+        self.ror = 0.0        # 평균단가 대비 수익률
         self.pror = -1       # 직전 매수가 대비 수익률
         self.dcl = dcl      # 분할매수 최소 미만 수익률 
         self.mtp = mtp      # 청산시 최소 수익률
@@ -42,13 +43,16 @@ class BinanceBot():
         df = df.astype('float')
         df['Time'] = pd.to_datetime(df['Time'], unit='ms')
         return df
+    
+    async def get_current_price(self):
+        return self.client.get_symbol_ticker(symbol=self.ticker)['price']
 
     async def get_balance(self):
         # 현재 USDT
         account = self.client.futures_account()
-        for balance in account:
+        for balance in account['assets']:
             if balance['asset'] == 'USDT':
-                return balance['balance']
+                return balance['marginBalance']
             
     async def get_futures_position(self):
         # 현재 포지션
@@ -56,7 +60,14 @@ class BinanceBot():
         for position in position_info:
             if position['symbol'] == self.ticker:
                 return position['positionAmt']
-        return 0  # 해당 심볼의 포지션을 찾지 못한 경우 0을 반환
+        return 0.0  # 해당 심볼의 포지션을 찾지 못한 경우 0을 반환
+    
+    async def get_precision(client, symbol):
+        # Base Asset Precision
+        exchange_info = client.get_exchange_info()
+        for symbol_info in exchange_info['symbols']:
+            if symbol_info['symbol'] == symbol:
+                return await symbol_info['baseAssetPrecision']
 
     async def set_strategy(self, st):
         # 매매 전략 설정
@@ -68,51 +79,53 @@ class BinanceBot():
 
     async def trading(self):
         # 매매 시작
-        self.balance = self.get_balance()
+        self.balance = await self.get_balance()
         self.on_trading = True
         while self.on_trading:
             try:
-                time.sleep(1)
+                time.sleep(10)
                 now = datetime.datetime.now()
-                df = self.get_ohlcv(client=self.client,ticker=self.ticker,interval=self.interval,limit=50)
+                df = await self.get_ohlcv(limit=50)
                 st = self.strategy(df)
                 self.current_action = st['Action'].iloc[-1]
                 print(f"{now}, {self.ticker}:{df['Close'].iloc[-1]}$, {self.current_action}")
 
                 if self.current_action != self.prev_action:
-                    price = self.client.get_symbol_ticker(symbol=self.ticker)['price']   # 현재가
+                    price = await self.get_current_price()   # 현재가
                     self.pror = (price-self.pre_price) / self.pre_price if self.pre_price!=None else -1
 
                     # 매수 신호
                     if self.current_action == 'Long' and self.dca_level < len(self.dca_levels) and self.pror < self.dcl:
                         buy_rate = self.dca_levels[self.dca_level]
-                        self.balance = self.get_balance()
+                        self.balance = await self.get_balance()
                         buy_quantity = self.balance * buy_rate / price * (1-self.fee)
                         self.pre_price = price
                         self.dca_level += 1
-                        self.client.futures_create_order(
+                        await self.client.futures_create_order(
                             symbol=self.ticker,
                             timeInForce='GTC',
                             type='MARKET',  # LIMIT, MARKET
                             side='BUY',
+                            positionSide='LONG',
                             quantity=buy_quantity,
                         )
-                        self.balance = self.get_balance()
+                        self.balance = await self.get_balance()
                     
                     # 매도 신호
                     elif self.current_action == 'Short' and self.dca_level > 0 and (self.pror > self.mtp or self.pror < self.stl):
                         sell_rate = 1
-                        sell_quantity = self.get_futures_position() * sell_rate
+                        sell_quantity = await self.get_futures_position() * sell_rate
                         self.pre_price = None
                         self.dca_level = 0
-                        self.client.futures_create_order(
+                        await self.client.futures_create_order(
                             symbol=self.ticker,
                             timeInForce='GTC',
                             type='MARKET',
                             side='SELL',
+                            positionside='SHORT',
                             quantity=sell_quantity,
                         )
-                        self.balance = self.get_balance()
+                        self.balance = await self.get_balance()
 
             except BinanceAPIException as e:
                 print(f"Binance Error \n{e}")
